6b371a82c41d393b2e9b660a8b4f17b3893800f0
Muhamad Surya Iksanudin <surya.kejawen@gmail.com>
2024-03-16 17:57:24 +0700

    add table level comparison

---
+++ pkg/command/detail.go
@@ -0,0 +1,94 @@
+package command
+
+import (
+	"github.com/ad3n/kmt/v2/pkg/config"
+	"github.com/ad3n/kmt/v2/pkg/db"
+
+	"github.com/fatih/color"
+)
+
+type detail struct {
+	config       config.Migration
+	boldFont     *color.Color
+	errorColor   *color.Color
+	successColor *color.Color
+}
+
+func NewDetail(config config.Migration) detail {
+	return detail{
+		config:       config,
+		boldFont:     color.New(color.Bold),
+		errorColor:   color.New(color.FgRed),
+		successColor: color.New(color.FgGreen),
+	}
+}
+
+func (d detail) Describe(table string, schema string, connection string) map[string]db.Column {
+	cfg, ok := d.config.Connections[connection]
+	if !ok {
+		d.errorColor.Printf("Database connection '%s' not found\n", d.boldFont.Sprint(connection))
+
+		return nil
+	}
+
+	conn, err := config.NewConnection(cfg)
+	if err != nil {
+		return nil
+	}
+
+	return db.NewTable("", cfg, conn).Detail(table)
+}
+
+func (d detail) Compare(table string, schema string, db1 string, db2 string) map[string]db.Compare {
+	cfg, ok := d.config.Connections[db1]
+	if !ok {
+		d.errorColor.Printf("Database connection '%s' not found\n", d.boldFont.Sprint(db1))
+
+		return nil
+	}
+
+	conn, err := config.NewConnection(cfg)
+	if err != nil {
+		return nil
+	}
+
+	compare := map[string]db.Compare{}
+
+	tdb1 := db.NewTable("", cfg, conn).Detail(table)
+	for k, v := range tdb1 {
+		compare[k] = db.Compare{
+			Table1: db.Column{
+				DataType:     v.DataType,
+				DefaultValue: v.DefaultValue,
+				Nullable:     v.Nullable,
+			},
+			Table2: db.Column{},
+		}
+	}
+
+	cfg, ok = d.config.Connections[db2]
+	if !ok {
+		d.errorColor.Printf("Database connection '%s' not found\n", d.boldFont.Sprint(db2))
+
+		return nil
+	}
+
+	conn, err = config.NewConnection(cfg)
+	if err != nil {
+		return nil
+	}
+
+	tdb2 := db.NewTable("", cfg, conn).Detail(table)
+	for k, v := range tdb2 {
+		cmp := compare[k]
+		cmp.Table2 = db.Column{
+			DataType:     v.DataType,
+			DefaultValue: v.DefaultValue,
+			Nullable:     v.Nullable,
+		}
+
+		compare[k] = cmp
+	}
+
+	return compare
+}
